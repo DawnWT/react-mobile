046774ad9358f1fd44895725a98ff60a317bbc1a
DawnWT <dwnssb@gmail.com>
2023-12-11 18:42:17 +0100

    feat: create home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,38 +1,150 @@
 import * as React from 'react'
-import { View, TextInput, Text, StyleSheet, Image } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { AppButton } from '../components/button'
+import { View, TextInput, Text, StyleSheet, Image, SafeAreaView } from 'react-native'
+import { useFakeStoreGetProduct } from '../hooks/useFakeStoreApi'
+import { Dropdown } from 'react-native-element-dropdown'
+import { ProductCard } from '../components/productCard'
+import { StatusBar } from 'expo-status-bar'
+import SVGCart from '../components/cartSVG'
+import { FlatList } from 'react-native-gesture-handler'
+
+const addressDropdownDatas = [{ address: 'Green Way 3000, Sylhet', id: 1 }]
+
+const withinDropdownDatas = [
+  { within: '1 Hour', id: 1 },
+  { within: '2 Hour', id: 2 },
+  { within: '3 Hour', id: 3 },
+]
 
 export const HomePage = () => {
-  const navigation = useNavigation()
+  const { data } = useFakeStoreGetProduct()
 
   return (
-    <View style={styles.header}>
-      <Text>Hey, Halal</Text>
-      <Image source={require('../../medias/icon/cart_icon.png')} />
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <View style={{ display: 'flex', flexDirection: 'row' }}>
+          <Text style={{ fontSize: 22, fontWeight: '600', color: '#F8F9FB', flexGrow: 1 }}>Hey, Halal</Text>
+          <SVGCart />
+        </View>
 
-      <View style={styles.searchBar}>
-        <TextInput style={styles.searchBarInput} placeholder="Search Products or store" />
+        <View style={styles.searchBar}>
+          <Image source={require('../../medias/icon/Search_Icon.png')} />
+          <TextInput
+            style={styles.searchBarInput}
+            placeholder="Search Products or store"
+            placeholderTextColor="#8891A5"
+          />
+        </View>
+
+        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View>
+            <Text style={styles.dropdownLabel}>delivery to</Text>
+            <Dropdown
+              data={addressDropdownDatas}
+              labelField="address"
+              valueField="id"
+              placeholder={'delivery to'.toUpperCase()}
+              value={addressDropdownDatas[0]}
+              onChange={() => void 0}
+              style={{ width: 180 }}
+              selectedTextStyle={styles.dropdownSelected}
+              iconColor="#B2BBCE"
+            />
+          </View>
+          <View>
+            <Text style={styles.dropdownLabel}>Within</Text>
+            <Dropdown
+              data={withinDropdownDatas}
+              labelField="within"
+              valueField="id"
+              value={withinDropdownDatas[0]}
+              onChange={() => void 0}
+              style={{ width: 70 }}
+              selectedTextStyle={styles.dropdownSelected}
+              iconColor="#B2BBCE"
+            />
+          </View>
+        </View>
+      </View>
+      <View style={styles.body}>
+        {data && (
+          <SafeAreaView>
+            <FlatList
+              style={styles.bodyContainer}
+              data={data}
+              renderItem={(data) => <ProductCard product={data.item} />}
+              keyExtractor={(data) => data.title}
+              numColumns={2}
+              columnWrapperStyle={{ gap: 20, marginBottom: 10 }}
+            />
+          </SafeAreaView>
+        )}
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  header: {
+  container: {
     backgroundColor: '#2A4BA0',
-    padding: 16,
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 40,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 30,
+    backgroundColor: '#2A4BA0',
+    padding: 20,
+    paddingBottom: 12,
   },
   searchBar: {
+    display: 'flex',
+    flexDirection: 'row',
     backgroundColor: '#153075',
-    borderRadius: 45,
-    padding: 8,
+    borderRadius: 28,
+    paddingVertical: 12,
+    paddingHorizontal: 28,
+    alignItems: 'center',
+    gap: 12,
   },
   searchBarInput: {
-    width: 335,
-    height: 56,
+    flexGrow: 1,
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#F8F9FB',
+    // width: 335,
+    // height: 56,
+  },
+  dropdownSelected: {
+    color: '#F8F9FB',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  dropdownLabel: {
+    color: '#F8F9FB',
+    opacity: 0.5,
+    fontSize: 11,
+    fontWeight: '800',
+    letterSpacing: 0.22,
+    textTransform: 'uppercase',
   },
   body: {
-    // Styles pour le reste du corps de la page
+    backgroundColor: '#FFF',
+    flexGrow: 1,
+    marginBottom: 223,
+  },
+  bodyContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    // display: 'flex',
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
+    // paddingHorizontal: 20,
+    // rowGap: 20,
+    // columnGap: 10,
+    // justifyContent: 'space-between',
   },
 })
